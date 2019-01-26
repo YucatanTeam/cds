101173f821434e25d8143be1eb8216ac82e7cbc7
javad asadi <force1267@gmail.com>
2019-01-26 22:56:52 +0330

    signup and login strategies

--- server/db.js
+++ server/db.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 const db = {
     connection: null,
     api: {
@@ -13,26 +15,73 @@ const db = {
                 }
             });
         },
-        getUserByUserPass(user, pass, cb) { // cb(err, user)
-            db.connection.query(`SELECT * FROM user WHERE username = ? AND password = ?`, [user, pass], (err, rows) => {
-                if(rows.length === 1) {
-                    cb(err, rows[0]);
+        getUserByEmailPass(email, password, cb) { // callback with email and password from our form
+            console.log(`finding ${email}:${password}`)
+            db.connection.query("SELECT * FROM `user` WHERE `email` = ?", [email], function (err, rows) {
+                console.log(`found rows`, rows)
+                if (err)
+                    return cb(err);
+                if (!rows.length) {
+                    return cb(null, false);
+                }
+                console.log(`no errors`)
+                // bcrypt appends the 'salt' to the 'hash' so we dont need to store the salt.
+                bcrypt.compare(password, rows[0].password, function(err, res) {
+                    if(res) {
+                        // Passwords match
+                        console.log(`password match`)
+                        return cb(null, rows[0]);
+                    } else {
+                        // Passwords don't match
+                        console.log(`wrong password`)
+                        return cb(null, false);
+                    }
+                });
+            });
+        },
+        addUser(email, password, cb) {
+            // find a user whose email is the same as the forms email
+            // we are checking to see if the user trying to login already exists
+            db.connection.query("SELECT * FROM user WHERE email = ?", [email], function (err, rows) {
+                if (err)
+                    return cb(err);
+                if (rows.length) {
+                    return cb(null, false);
                 } else {
-                    cb(err, false);
+                    // if there is no user with that email
+                    // create the user
+                    var newUserMysql = {}
+
+                    newUserMysql.email = email;
+
+                    bcrypt.hash(password, 10, function(err, hash) {
+                        // hash contains the salt (salt.hash) so we dont need to store salt
+                        // Store hash in database
+                        var insertQuery = "INSERT INTO users ( email, password, access ) values ( ?, ?, 2 )";
+                        db.connection.query(insertQuery, [email, hash], function (err, rows) {
+                            newUserMysql.id = rows.insertId;
+                            return cb(null, newUserMysql);
+                        });
+                    });
+                    
                 }
             });
         },
-        addUser(user ,cb) {
-            db.connection.query(`INSERT INTO user(firstname, lastname, username, phone, access, password) VALUES (?,?,?,?,?,?)`, [user.firstname,user.lastname,user.username,user.phone,parseInt(user.access),user.password], cb);
-        },
         editUser(user ,cb) {
-            db.connection.query(`UPDATE user SET firstname = ?, lastname = ?, username = ?, phone = ?, access = ?, password = ? WHERE id = ?`, [user.firstname,user.lastname,user.username,user.phone,parseInt(user.access),user.password,user.id], cb);
+            var password = user.password;
+            bcrypt.hash(password, 10, function(err, hash) {
+                user.password = hash;
+                db.connection.query(`UPDATE user SET firstname = ?, lastname = ?, username = ?, phone = ?, access = ?, password = ? WHERE id = ?`, [user.firstname,user.lastname,user.username,user.phone,parseInt(user.access),user.password,user.id], cb);
+            });
         },
         deleteUser(id, cb) {
             db.connection.query(`DELETE FROM user WHERE id = ?`, [id], cb);
         },
         changePassword(id, pass, cb) {
-            db.connection.query("UPDATE user SET password = ? WHERE id = ?", [pass, id], cb);
+            bcrypt.hash(pass, 10, function(err, hash) {
+                db.connection.query("UPDATE user SET password = ? WHERE id = ?", [hash, id], cb);
+            });
+            
         }
     }
 }
