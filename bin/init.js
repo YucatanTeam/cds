3c61e5ef65814da635d014185db0d3fd45ae4139
erfanarefi <ea_pain@yahoo.com>
2019-01-26 23:12:20 +0330

    init table feature added

--- bin/init.js
+++ bin/init.js
@@ -5,6 +5,7 @@
 
 
 const mysql = require("mysql");
+const cuid = require("cuid"); // use this to create a cuid in insertaion ops
 
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || null;
@@ -24,8 +25,71 @@ const connection = mysql.createConnection({
 connection.connect(err => {
     if(err) return console.error(err);
     console.log(`database is connected to ${DB_USER}@${DB_HOST}/${DB_NAME}`);
-
-    connection.query("CREATE TABLE test(a TEXT)" ,[] ,console.log);
-
+    
     // TODO make init queries
+    
+
+
+    /* -------------------------
+       [ insertion / creation ]
+    
+        // db creation...
+    // connection.query(`CREATE SCHEMA ${DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_persian_ci ;`)
+    */
+
+
+    // ------------------------------------------------------------
+    // post table
+    // ...
+    connection.query(`CREATE TABLE IF NOT EXISTS post (
+                                    id INT AUTO_INCREMENT PRIMARY KEY, 
+                                    title VARCHAR(255) NOT NULL,
+                                    en_title VARCHAR(255) NOT NULL,
+                                    content TEXT NOT NULL,
+                                    en_content TEXT NOT NULL,
+                                    tags JSON NOT NULL,
+                                    en_tags JSON NOT NULL,
+                                    status TINYINT DEFAULT 0,
+                                    slug TEXT NOT NULL,
+                                    en_slug TEXT NOT NULL,
+                                    cuid VARCHAR(100) NOT NULL,
+                                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                    cover VARCHAR(1024) DEFAULT NULL)ENGINE=INNODB;` ,[] ,console.log);
+
+     // TODO insertaion bug!!! GOT SOME ERROR
+    connection.query(`INSERT INTO post VALUES(
+                        'اوین پست',
+                        'first post',
+                        'این اولین پست است',
+                        'this is the first post',
+                        JSON_ARRAY('ویزا', 'کانادا'),
+                        JSON_ARRAY('visa', 'canada'),
+                         'اولین-پست',
+                         'first-post',
+                         ${cuid()}
+                    );` ,[] ,console.log);
+    // ------------------------------------------------------------
+    // comment table
+    // ...
+    connection.query(`CREATE TABLE IF NOT EXISTS comment (
+                id INT AUTO_INCREMENT PRIMARY KEY, 
+                post_id INT,
+                content TEXT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                status TINYINT DEFAULT 0,
+                cuid VARCHAR(100) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                INDEX post_ind (post_id),
+                FOREIGN KEY (post_id)
+                    REFERENCES post(id)
+                    ON DELETE CASCADE)ENGINE=INNODB;` ,[] ,console.log);
+
+
+    // ------------------------------------------------------------
+    // user table
+    // ...
+
 });
