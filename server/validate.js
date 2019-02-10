96bcf161f21887aa725c1dd3f27d8746509605ac
javad asadi <force1267@gmail.com>
2019-02-11 01:59:32 +0330

    validator functions

---
+++ server/validate.js
@@ -0,0 +1,22 @@
+
+
+module.exports = {
+    password(target) {
+        // TODO
+        return true;
+    },
+    username(target) {
+        // TODO
+        return true;
+    },
+    email(target) {
+        // TODO
+        return true;
+    },
+    avatar(target) {
+        // TODO
+        // avatar must be jpeg or png
+        // check target.type ( maybe dont trust it )
+        return true;
+    }
+}
