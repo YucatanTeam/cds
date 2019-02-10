ee081962e78f166a6457676cc923d9fc85341b97
javad asadi <force1267@gmail.com>
2019-02-11 01:59:15 +0330

    restructure db.api

--- server/db.js
+++ server/db.js
@@ -5,81 +5,105 @@ const db = {
     connection: null,
     api: {
         /* --------------------
+            SERVER API
+        */
+        server: {
+            error: {
+                add(error, cb) {
+                    // status : 1 fresh, 2 seen, 3 solved
+                    db.connection.query(`INSERT INTO error(msg, status) VALUES(?, 1)`, [error], cb ? cb : e=>e);
+                },
+                handled(errorId, cb) {
+                    db.connection.query(`UPDATE error SET status = 3 WHERE id = ?`, [errorId], cb ? cb : e=>e);
+                },
+                seen(errorId, cb) {
+                    db.connection.query(`UPDATE error SET status = 2 WHERE id = ?`, [errorId], cb ? cb : e=>e);
+                },
+                list(cb) {
+                    db.connection.query(`SELECT * FROM error`, [], cb);
+                },
+            }
+        },
+        /* --------------------
             USER API
         */
-        getAllUsers(cb) { // cb(err, user)
-            db.connection.query(`SELECT * FROM user`, [], cb);
-        },
-        getUserById(id, cb) { // cb(err, user)
-            db.connection.query(`SELECT * FROM user WHERE id = ?`, [id], (err, rows) => {
-                if(rows.length === 1) {
-                    return cb(err, rows[0]);
-                } else {
-                    return cb(err, false);
-                }
-            });
-        },
-        getUserByEmailPass(email, password, cb) { // callback with email and password from our form
-            db.connection.query("SELECT * FROM `user` WHERE `email` = ?", [email], function (err, rows) {
-                if (err)
-                    return cb(err);
-                if (!rows.length) {
-                    return cb(null, false);
-                }
-                // hash function appends the 'salt' to the 'hash' so we dont need to store the salt.
-                safe.compare(password, rows[0].password, function(err, res) {
-                    if(res) {
-                        // Passwords match
-                        return cb(null, rows[0]);
-                    } else{
-                        // Passwords don't match
+        user: {
+            all(cb) { // cb(err, user)
+                db.connection.query(`SELECT * FROM user`, [], cb);
+            },
+            getById(id, cb) { // cb(err, user)
+                db.connection.query(`SELECT * FROM user WHERE id = ?`, [id], (err, rows) => {
+                    if(rows.length === 1) {
+                        return cb(err, rows[0]);
+                    } else {
                         return cb(err, false);
                     }
+                });
+            },
+            getByEmailPass(email, password, cb) { // callback with email and password from our form
+                db.connection.query("SELECT * FROM `user` WHERE `email` = ?", [email], function (err, rows) {
+                    if (err)
+                        return cb(err);
+                    if (!rows.length) {
+                        return cb(null, false);
+                    }
+                    // hash function appends the 'salt' to the 'hash' so we dont need to store the salt.
+                    safe.compare(password, rows[0].password, function(err, res) {
+                        if(res) {
+                            // Passwords match
+                            return cb(null, rows[0]);
+                        } else{
+                            // Passwords don't match
+                            return cb(err, false);
+                        }
+                    })
+                });
+            },
+            add(email, password, cb) {
+                // find a user whose email is the same as the forms email
+                // we are checking to see if the user trying to login already exists
+                db.connection.query("SELECT * FROM user WHERE email = ?", [email], function (err, rows) {
+                    if (err)
+                        return cb(err);
+                    if (rows.length) {
+                        return cb(null, false);
+                    } else {
+                        // if there is no user with that email
+                        // create the user
+                        var newUserMysql = {}
+    
+                        newUserMysql.email = email;
+    
+                        safe.hash(password, 10, function(err, saltdk) {
+                            // saltdk contains the salt and hash (salt + hash) so we dont need to store salt
+                            // Store saltdk in database
+                            var insertQuery = "INSERT INTO users ( email, password, access ) values ( ?, ?, 2 )";
+                            db.connection.query(insertQuery, [email, saltdk], function (err, rows) {
+                                newUserMysql.id = rows.insertId;
+                                return cb(null, newUserMysql);
+                           }) 
+                        });
+                    }
+                });
+            },
+            update(user ,cb) {
+                safe.hash(user.password, 10, function(err, saltdk) {
+                    user.password = saltdk;
+                    db.connection.query(`UPDATE user SET firstname = ?, lastname = ?, access = ?, password = ? WHERE id = ?`, [user.firstname,user.lastname,parseInt(user.access),user.password,user.id], cb);
                 })
-            });
-        },
-        addUser(email, password, cb) {
-            // find a user whose email is the same as the forms email
-            // we are checking to see if the user trying to login already exists
-            db.connection.query("SELECT * FROM user WHERE email = ?", [email], function (err, rows) {
-                if (err)
-                    return cb(err);
-                if (rows.length) {
-                    return cb(null, false);
-                } else {
-                    // if there is no user with that email
-                    // create the user
-                    var newUserMysql = {}
-
-                    newUserMysql.email = email;
-
-                    safe.hash(password, 10, function(err, saltdk) {
-                        // saltdk contains the salt and hash (salt + hash) so we dont need to store salt
-                        // Store saltdk in database
-                        var insertQuery = "INSERT INTO users ( email, password, access ) values ( ?, ?, 2 )";
-                        db.connection.query(insertQuery, [email, saltdk], function (err, rows) {
-                            newUserMysql.id = rows.insertId;
-                            return cb(null, newUserMysql);
-                       }) 
-                    });
-                }
-            });
-        },
-        editUser(user ,cb) {
-            var password = user.password;
-            safe.hash(password, 10, function(err, saltdk) {
-                user.password = saltdk;
-                db.connection.query(`UPDATE user SET firstname = ?, lastname = ?, username = ?, phone = ?, access = ?, password = ? WHERE id = ?`, [user.firstname,user.lastname,user.username,user.phone,parseInt(user.access),user.password,user.id], cb);
-            })
-        },
-        deleteUser(id, cb) {
-            db.connection.query(`DELETE FROM user WHERE id = ?`, [id], cb);
-        },
-        changePassword(id, pass, cb) {
-            safe.hash(pass, 10, function(err, saltdk) {
-                db.connection.query("UPDATE user SET password = ? WHERE id = ?", [saltdk, id], cb);
-            });
-            
+            },
+            changeAvatar(id, avatar, cb) {
+                db.connection.query(`UPDATE user SET avatar = ? WHERE id = ?`, [avatar, id], cb);
+            },
+            remove(id, cb) {
+                db.connection.query(`DELETE FROM user WHERE id = ?`, [id], cb);
+            },
+            changePassword(id, pass, cb) {
+                safe.hash(pass, 10, function(err, saltdk) {
+                    db.connection.query("UPDATE user SET password = ? WHERE id = ?", [saltdk, id], cb);
+                });
+                
+            },
         },
         /* --------------------
             COMMENTS API
