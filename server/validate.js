438e85beb1823f0fd0ed5f06d17748b0f494bfd0
javad asadi <force1267@gmail.com>
2019-02-15 03:42:47 +0330

    firstname and lastname validation

--- server/validate.js
+++ server/validate.js
@@ -1,13 +1,16 @@
 
 
-module.exports = {
+module.exports = validate = {
     password(target) {
         // TODO
         return true;
     },
-    username(target) {
+    firstname(target) {
         // TODO
         return true;
+    },
+    lastname(target) {
+        return validate.firstname(target);
     },
     email(target) {
         // TODO
