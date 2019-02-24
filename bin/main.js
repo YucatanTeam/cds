d39fa3909a3f121266f48c2f0136da389283140f
@Mafi13 <khashayar.mafi75@gmail.com>
2019-02-24 16:43:25 +0330

    port changed to 5000 cause of apache issue!

--- bin/main.js
+++ bin/main.js
@@ -16,7 +16,7 @@ const {app, db} = require('../server/index.js')
 
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_USER = process.env.DB_USER || 'root';
-const DB_PASS = process.env.DB_PASS || 'Qwe%$[rty]*@;123'; // wildonion password
+const DB_PASS = process.env.DB_PASS || 'root';
 const DB_NAME = process.env.DB_NAME || 'cds';
 
 const mysql = require("mysql");
@@ -34,7 +34,7 @@ db.connection.connect(err => {
 
 
 const PID = process.pid;
-const PORT = process.env.PORT || process.argv[2] || 8080; // we'll use this in production
+const PORT = process.env.PORT || process.argv[2] || 5000; // we'll use this in production
 const SECRET = process.env.SECRET || "force"; // we'll use this in production
 
 app.listen(PORT)
