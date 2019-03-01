5913e2bcf46ea4e9265e01ed17bb5e0f3ace2cbc
erfanarefi <ea_pain@yahoo.com>
2019-03-02 00:29:37 +0330

    fixed bugs of server and ui

--- bin/main.js
+++ bin/main.js
@@ -16,7 +16,7 @@ const {app, db} = require('../server/index.js')
 
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_USER = process.env.DB_USER || 'root';
-const DB_PASS = process.env.DB_PASS || 'root';
+const DB_PASS = process.env.DB_PASS || 'Qwe%$[rty]*@;123';
 const DB_NAME = process.env.DB_NAME || 'cds';
 
 const mysql = require("mysql");
