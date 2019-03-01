34f903ade6e4bf4571399707b80b25a5b2c336ac
@Mafi13 <khashayar.mafi75@gmail.com>
2019-03-02 02:14:46 +0330

    telebot

--- server/Tbot.js
+++ server/Tbot.js
@@ -1,8 +1,14 @@
+var request = require('request');
 
-
-
-exports.bot = ({app}) => function bot(photoUrl,caption){
-    app.post('/telegram/bot',function(req,res){
-        
-    });
+module.export = function bot(Photo,Caption){
+    request({
+        method:"POST",
+        url:'https://api.telegram.org/bot740062919:AAFzZCd9oqNpwVr9X9jF1j8XTECYiEwF5Ck/sendPhoto',
+        formData:{
+            photo:`${Photo}`,
+            caption:`${Caption}`
+        }
+    },function(err,res,body){
+        if(err) throw err
+    })
 }
