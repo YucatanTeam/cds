f45285b7ca85e135fb988478a2353e2739c3d2ad
javad asadi <force1267@gmail.com>
2019-03-08 18:44:04 +0330

    listen now

--- bin/main.js
+++ bin/main.js
@@ -37,8 +37,8 @@ const PID = process.pid;
 const PORT = process.env.PORT || process.argv[2] || 8080; // we'll use this in production
 const SECRET = process.env.SECRET || "force"; // we'll use this in production
 
-// app.listen(PORT)
-app.listen()
+app.listen(PORT)
+// app.listen()
 
 console.log(`server is running on http://127.0.0.1:${PORT}/`)
 console.log(`PID ${PID}`)
